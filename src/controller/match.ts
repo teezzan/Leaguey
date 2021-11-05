7564c6109f1dcf0917cf806a0dfbe43e37ef7e78
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-11-05 06:43:38 +0100

    added new fuctions

--- src/controller/match.ts
+++ src/controller/match.ts
@@ -21,14 +21,33 @@ export default class MatchController {
     @summary("List Available league and season pairs")
     @description("List the league and season pairs for which there are results available.")
     public static async ListPairs(ctx: Context): Promise<void> {
+        let output_tags: Array<{
+            div: string,
+            season: string
+        }> = [];
 
-        const matches = await getManager()
+        let temp_output: Array<string> = [];
+
+        const matches: Array<{
+            match_div: string,
+            match_season: string
+        }> = await getManager()
             .createQueryBuilder(Match, "match")
             .select(["match.div", "match.season"])
             .execute();
 
+
+        for (let i = 0; i < matches.length; i++) {
+            const match = matches[i]
+            let div = match.match_div;
+            let season = `${match.match_season.substr(0, 4)}-20${match.match_season.substr(4)}`
+            temp_output.push(`${div} ${season}`);
+        }
+
+        let output= new Set(temp_output);
+
         ctx.status = 200;
-        ctx.body = { matches };
+        ctx.body = { matches: [...output] };
         return;
     }
 }
